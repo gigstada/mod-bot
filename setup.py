0e88494ef02d084445f374e4a7e802336c16ed18
Adam Gigstad <adam.gigstad@gmail.com>
2023-09-05 09:05:32 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='mod-bot-ai',
     version='0.1.0',
     packages=find_packages(),
-    py_modules=["mod"],
+    py_modules=["mod", "fn_loop"],
     install_requires=[i.strip() for i in open("requirements.txt").readlines()],
     entry_points={
         'console_scripts': ['mod=mod:main']
