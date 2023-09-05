ed6085b1bce7601184ee399780f2ed85c6894417
Adam Gigstad <adam.gigstad@gmail.com>
2023-09-05 08:48:05 -0700

    First draft of setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mod-bot-ai',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[i.strip() for i in open("requirements.txt").readlines()],
+    entry_points={
+        'console_scripts': ['mod=mod:main']
+    }
+)
